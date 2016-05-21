cfb80ab35de440668b8dbddcb806cde8f340f807
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-21 19:02:30 +0300

    Update scaletest.m

--- scaletest.m
+++ scaletest.m
@@ -1,6 +1,16 @@
 f = scale; d = 1; k = 8;
+f = scale; d = 1; k = 8;
 fs = d/(128*k); t = fs:fs:d;
-for i=1:length(f)
-    tone = signal( f(i), t);
-    sound(tone), pause(0.5)
+for i=1 + floor(option/4):length(f)
+    switch option
+    case 1,
+        tone = signal( f(1:i), t);
+    case 2,
+        tone = signal( f(i), t);
+     case 3, % odd reference scale
+         if mod(i,2), tone = signal( f(1:2:i), t);  end
+      case 4, % even reference scale
+          if mod(i,2)==0, tone = signal( f(2:2:i), t); end
+    end
+    sound(tone), pause(0.25)
 end
