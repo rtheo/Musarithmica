26d54de94a1406c1d7c890a4ada67d86599a3faa
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-22 10:50:59 +0300

    Update signalbank.m

--- signalbank.m
+++ signalbank.m
@@ -1,11 +1,12 @@
 function signalbank(d, k, low)
-% d=3; k=64; 
+% try d=3; k=128; 
+clc, close all
 fs = d/(128*k); t = fs:fs:d; 
 f = scale; k2 = 6*low; k1 = k2 - 5;
 words = fliplr( ff2n(6) );
 for i=1:64 
      w=words(i,:); 
      if sum(w)==3 && sum(w(1:2))*sum(w(3:4))*sum(w(5:6)) 
-         s = signal( (f(k1:k2).*w), t ); sound(s, 5/fs), pause(0.1)
-     end, 
+         s = signal( (f(k1:k2).*w), t ); 
+     end, plot(s), sound(s), pause(0.2)
  end
