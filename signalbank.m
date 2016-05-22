f940c44187cfc064661229a9a313232641d2f623
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-22 11:03:19 +0300

    Update signalbank.m

--- signalbank.m
+++ signalbank.m
@@ -7,7 +7,8 @@ words = fliplr( ff2n(6) );
 for i=1:64 
      w=words(i,:); 
      if sum(w)==3 && sum(w(1:2))*sum(w(3:4))*sum(w(5:6)) 
-         s = signal( (f(k1:k2).*w), t ); psd(s), pause
-         plot(s), sound(s)
+         s = signal( (f(k1:k2).*w), t ); 
+         psd(s), title(['word: ',num2str(w)]), pause
+         sound(s)
      end 
  end
