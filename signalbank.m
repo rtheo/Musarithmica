0b7ad5cdc33fec5245f087f55ef6293e0c7a23ee
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-22 16:02:26 +0300

    Update signalbank.m

--- signalbank.m
+++ signalbank.m
@@ -4,11 +4,11 @@ clc, close all
 fs = d/(128*k); t = fs:fs:d; 
 f = scale; k2 = 6*low; k1 = k2 - 5;
 words = fliplr( ff2n(6) );
+k = 0;
 for i=1:64 
      w=words(i,:); 
-     if sum(w)==3 && sum(w(1:2))*sum(w(3:4))*sum(w(5:6)) 
-         s = signal( (f(k1:k2).*w), t ); 
-         psd(s), title(['word: ',num2str(w)]), pause
-         sound(s)
+     if sum(w)==3 && sum(w(1:2))*sum(w(3:4))*sum(w(5:6))==1 
+         k = k + 1; s = signal( (f(k1:k2).*w), t ); y = sum(s, 1);
+         sound(y), psd(y), title(['word: ',num2str(w)]), pause
      end 
  end
