8031a545f60f5e16910fe5920328759b9726dfc2
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-23 20:02:40 +0300

    Update ZellenKlavier.m

--- ZellenKlavier.m
+++ ZellenKlavier.m
@@ -1,4 +1,10 @@
-function WellenKlavier( cells, rule, steps, cutoff, duration, fs, fsplay )
+function WellenKlavier( cbytes, rule, steps, cutoff, duration, fs, fsplay )
+% Cellular Piano: the sound of 1D CA isospectrally encoded in freq. domain 
+% cbytes := array of 2 32-bit large integers or a large 64-bit integer for cell array, 
+% depending on the machine precision.
+% rule: = byte as integer, steps:= total evolution time
+% A 'rule' code is interpreted as a unique if...else... tree or switch/case... 'program'
+% duration:= time evolution of single pitch tones
 if nargin < 7, fsplay = 44100; end
 if nargin < 6, fs = 2^13; end
 if nargin < 5, duration = 1; end
@@ -11,8 +17,9 @@ f1 = floor(freqs(1:44)) + floor( 10*mod( freqs(1:44), 1 ) )/10;
 f2 =  floor(freqs(46:128)) + floor( 10*mod( freqs(46:128), 1 ) )/10;
 freqs = [f1, f0, f2]; freqs = freqs( 1+mod( (0:127), cutoff) ); % apply cutoff for random compositions
 % Expand cells for isentropic encoding
-clen = length(cells); if clen<64, cells = [cells, zeros(1, 64-clen)]; end
-if clen > 64, error('Cell array length exceeding max. capacity of 64 bits'), end
+% You may comment line 22 in a 64-bit machine and increase exponents in 21 as (0:63)
+cells = ceil(mod(log2(bitxor(cbytes(1), cbytes(1)+2.^(0:31))), 1)); % division-free cell bits exractor 
+cells = [cells, ceil(mod(log2(bitxor(cbytes(2), cbytes(2)+2.^(0:31))), 1))];
 c = zeros(1,128); c(1:2:end) = 1; idx = 2*find( cells==1 )-1; c(idx) = 0; c(idx+1) = 1; 
 % Load signal bank with cell states and complement
 ifs = duration/fs; t=ifs:ifs:duration; 
@@ -35,9 +42,8 @@ for i=1:steps
         if rfilter,
             keys( k, : ) = ( flag>0 )*sbank(k, : ); 
             keys( k+1, : ) = ( flag<0 )*csbank( k+1, : ); 
-            %sound( [keys( k, : ) keys( k+1, : )], fsplay )
+            sound( [keys( k, : ) fliplr(keys( k, : ))], fsplay )            
         end        
-        sound( [keys( k, : ) keys( k+1, : )], fsplay )
         k = k + 1;
     end
 end
